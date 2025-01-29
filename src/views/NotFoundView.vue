bfb7f0fdefa97929b69961954b29a79e995a2f6b
RusHiiii <fdamiens@itnetwork.fr>
2025-01-29 20:58:18 +0100

    Add not found page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>not found</div>
+  <BreadCrumb text="Erreur" />
+
+  <div class="fullwidth-block">
+    <div class="container">
+      <div class="row">
+        <div class="content col-md-8">
+          <div class="post single">
+            <h2 class="entry-title">Oops - Page non trouvée :(</h2>
+            <div class="entry-content">
+              <p>
+                La page que vous demandez n'existe pas. Elle a peut-être été
+                déplacée. Revenez vers l'accueil pour accéder aux données.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
+import BreadCrumb from "@/components/common/BreadCrumb.vue";
+import {ROUTES} from "@/common/constant.ts";
 </script>
